49330357efdaac7f296f40f656ac29780e0bde87
StivenParra2001 <nevits2001@gmail.com>
2025-09-04 20:03:01 -0500

    se agrega

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 const { check } = require('express-validator');
 const { register, login } = require('../controllers/authController');
 
-
+router.post('/register', register);
 router.post('/login', login);
 
 module.exports = router;
